10b625f6ab62ab33bb1435590bb053e5a8bc5252
ramzoon <rohamrohani9033@gmail.com>
2025-09-30 22:14:20 +0330

    Add request feature to the bot Update requirenments.txt

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from telebot import TeleBot
 from dotenv import dotenv_values
+import requests
 
 # Config Variables
 CONFIG = dotenv_values()
 
 bot = TeleBot(CONFIG['API_KEY'])
+@bot.message_handler(commands=['start'])
+
+def hello_word(messege):
+    data = requests.get('https://api.github.com/users/r0h9m')
+
+    print(type(data.json()))
+    bot.send_message(messege.chat.id,'koni')
+
+bot.polling()
